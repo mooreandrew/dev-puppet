13444b2a7ce662e743f499c9268aeeb79b0a3aaa
Andrew Moore <mooreandrew@gmail.com>
2015-03-18 20:54:27 +0000

    Fixed missing production environment folder

--- provision-agent.sh
+++ provision-agent.sh
@@ -3,6 +3,8 @@ echo 172.16.42.50 puppet-master.home.net puppet >> /etc/hosts
 environment=$1
 r10k=$2
 
+sudo setenforce 0
+
 sed -i -e 's,keepcache=0,keepcache=1,g' /etc/yum.conf
 
 sudo yum -y install puppet
@@ -16,7 +18,7 @@ sshpass -p vagrant ssh -t vagrant@puppet-master.home.net -o StrictHostKeyCheckin
 sshpass -p vagrant ssh -t vagrant@puppet-master.home.net -o StrictHostKeyChecking=no "sudo puppet cert clean $HOSTNAME; exit 0"
 
 if [ $r10k = "true" ]; then
-  sshpass -p vagrant ssh -t vagrant@puppet-master.home.net -o StrictHostKeyChecking=no "sudo r10k deploy environment -p; exit 0"
+  sshpass -p vagrant ssh -t vagrant@puppet-master.home.net -o StrictHostKeyChecking=no "sudo r10k deploy environment -p; sudo mkdir /etc/puppet/environments/production; exit 0"
 fi
 
 puppet agent --test
